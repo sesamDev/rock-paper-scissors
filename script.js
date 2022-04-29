55d301b19de6f6f421004acf0e84c96a9a419956
Sebastian <sebastian.samils@gmail.com>
2022-04-29 16:38:29 +0200

    Refactored the code, split up playRound()

--- script.js
+++ script.js
@@ -14,28 +14,52 @@ function playerChoise(){
     return prompt("Please enter your selection, Rock, Paper or Scissors").toLowerCase()
 }
 
+function checkWinner(player, computer){
+        //Check to see winner
+        if(player == "Rock" && computer == "Scissors" || 
+        player == "Scissors" && computer == "Paper" ||
+        player == "Paper" && computer == "Rock"){
+            return "Player";
+        }else if(player === computer){
+            return "Tie";
+        }else{
+            return "Computer"
+        }
+}
+
+function updateScore(roundWinner){
+    if(roundWinner == "Player") return playerScore++;
+    if(roundWinner == "Computer") return computerScore++
+    if(roundWinner == "Tie") return console.log("It's a Tie! No points!")
+}
+
 function playRound(playerSelection, computerSelection){
     let playerChoise = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
     let computerChoice = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
     let winner = ""
 
-    if(playerChoise == "Rock" && computerChoice == "Scissors"){
-        console.log(`You win! ${playerChoise} beats ${computerChoice}`)
-        return winner = "Player"
-    }else if(playerChoise == "Scissors" && computerChoice == "Paper"){
-        console.log(`You win! ${playerChoise} beats ${computerChoice}`)
-        return winner = "Player"
-    }else if(playerChoise == "Paper" && computerChoice == "Rock"){
-        console.log(`You win! ${playerChoise} beats ${computerChoice}`)
-        return winner = "Player"
-    }else if(playerChoise == computerChoice){
-        console.log(`It's a tie! You both chose ${playerChoise}`)
-        return winner = "Tie"
-    }
-    else{
-        console.log(`You lose! ${computerChoice} beats ${playerChoise}`)
-        return winner = "Computer"
-    }
+    updateScore(checkWinner(playerChoise, computerChoice));
+    console.log("Score Player: " + playerScore + " Computer:" + computerScore);
+
+    // if(playerChoise == "Rock" && computerChoice == "Scissors"){
+    //     console.log(`You win! ${playerChoise} beats ${computerChoice}`)
+    //     return winner = "Player"
+    // }else if(playerChoise == "Scissors" && computerChoice == "Paper"){
+    //     console.log(`You win! ${playerChoise} beats ${computerChoice}`)
+    //     return winner = "Player"
+    // }else if(playerChoise == "Paper" && computerChoice == "Rock"){
+    //     console.log(`You win! ${playerChoise} beats ${computerChoice}`)
+    //     return winner = "Player"
+    // }else if(playerChoise == computerChoice){
+    //     console.log(`It's a tie! You both chose ${playerChoise}`)
+    //     return winner = "Tie"
+    // }
+    // else{
+    //     console.log(`You lose! ${computerChoice} beats ${playerChoise}`)
+    //     return winner = "Computer"
+    // }
+
+
 
 }
 
